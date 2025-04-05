6e63aeed71e070dc2773b8456dca3f29d939c0f6
mmarci96 <sarosdimarci@gmail.com>
2025-04-06 01:53:49 +0200

    just gooo

--- go-reverse-proxy/cmd/main.go
+++ go-reverse-proxy/cmd/main.go
@@ -11,4 +11,5 @@ func main() {
 	if err := server.Run(); err != nil {
 		log.Fatalf("could not start the server: %v", err)
 	}
+	select {}
 }
--- go-reverse-proxy/internal/server/server.go
+++ go-reverse-proxy/internal/server/server.go
@@ -8,28 +8,29 @@ import (
 	"github.com/mmarci96/go-ws-traffic-controller/go-reverse-proxy/internal/configs"
 )
 
+// Run starts server and listens on defined port
 func Run() error {
+	// load configurations from config file
 	config, err := configs.NewConfiguration()
 	if err != nil {
 		return fmt.Errorf("could not load configuration: %v", err)
 	}
+	// Creates a new router
 	mux := http.NewServeMux()
+	// Registering the healthcheck endpoint
 	mux.HandleFunc("/ping", ping)
-
+	// Iterating through the configuration resource and registering them
+	// into the router.
 	for _, resource := range config.Resources {
 		url, _ := url.Parse(resource.Destination_URL)
 		proxy := NewProxy(url)
 		mux.HandleFunc(resource.Endpoint, ProxyRequestHandler(proxy, url, resource.Endpoint))
 	}
-	addr := config.Server.Host + ":" + config.Server.Listen_port
-	fmt.Printf("[ TinyRP ] Started server on %s\n", addr)
+	fmt.Printf("[ TinyRP ] Server running on http://%s:%s\n", config.Server.Host, config.Server.Listen_port)
 
-	errorPageDir := "../html/error"
-	mux.Handle("/404", http.FileServer(http.Dir(errorPageDir)))
+	// Running proxy server
 	if err := http.ListenAndServe(config.Server.Host+":"+config.Server.Listen_port, mux); err != nil {
 		return fmt.Errorf("could not start the server: %v", err)
 	}
-	fmt.Printf("[ TinyRP ] Running on %s\n", addr)
-
 	return nil
 }

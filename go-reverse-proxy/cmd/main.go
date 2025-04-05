714e88c82215b33681ccca52c9d379006f747ded
mmarci96 <sarosdimarci@gmail.com>
2025-04-06 01:26:55 +0200

    checking how to serve static files

--- go-reverse-proxy/cmd/main.go
+++ go-reverse-proxy/cmd/main.go
@@ -8,10 +8,7 @@ import (
 
 func main() {
 	fmt.Println("Reverse proxy starting...")
-	addr, err := server.Run()
-	if err != nil {
+	if err := server.Run(); err != nil {
 		log.Fatalf("could not start the server: %v", err)
 	}
-	fmt.Printf("Proxy server running on:http://%s\n", addr)
-	select {}
 }
--- go-reverse-proxy/internal/server/server.go
+++ go-reverse-proxy/internal/server/server.go
@@ -8,32 +8,28 @@ import (
 	"github.com/mmarci96/go-ws-traffic-controller/go-reverse-proxy/internal/configs"
 )
 
-// Run starts server and listens on defined port
-func Run() (string, error) {
-	// load configurations from config file
+func Run() error {
 	config, err := configs.NewConfiguration()
 	if err != nil {
-		return "", fmt.Errorf("could not load configuration: %v", err)
+		return fmt.Errorf("could not load configuration: %v", err)
 	}
-	// Creates a new router
 	mux := http.NewServeMux()
-	// Registering the healthcheck endpoint
 	mux.HandleFunc("/ping", ping)
-	// Iterating through the configuration resource and registering them
-	// into the router.
+
 	for _, resource := range config.Resources {
 		url, _ := url.Parse(resource.Destination_URL)
 		proxy := NewProxy(url)
 		mux.HandleFunc(resource.Endpoint, ProxyRequestHandler(proxy, url, resource.Endpoint))
 	}
 	addr := config.Server.Host + ":" + config.Server.Listen_port
-	fmt.Printf("[ TinyRP ] Starting server on %s\n", addr)
+	fmt.Printf("[ TinyRP ] Started server on %s\n", addr)
 
-	// Running proxy server
+	errorPageDir := "../html/error"
+	mux.Handle("/404", http.FileServer(http.Dir(errorPageDir)))
 	if err := http.ListenAndServe(config.Server.Host+":"+config.Server.Listen_port, mux); err != nil {
-		return "", fmt.Errorf("could not start the server: %v", err)
+		return fmt.Errorf("could not start the server: %v", err)
 	}
 	fmt.Printf("[ TinyRP ] Running on %s\n", addr)
 
-	return addr, nil
+	return nil
 }
